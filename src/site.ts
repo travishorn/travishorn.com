ac8f919b19d04083f13919b13b3f6ecf335d57bd
Travis Horn <travis@travishorn.com>
2026-02-02 11:19:23 -0600

    Adjust titles and descriptions for SEO

--- src/site.ts
+++ src/site.ts
@@ -1,2 +1,2 @@
-export const siteTitle = "Travis Horn";
-export const siteDescription = "I'm a Data Scientist & Software Engineer. I have a passion for discovering and working with cutting-edge technology. I enjoy collaborating with others to solve problems.";
+export const siteTitle = "Travis Horn - Data Scientist and Software Engineer";
+export const siteDescription = "I'm a Data Scientist & Software Engineer passionate about cutting-edge technology. I collaborate with teams to solve complex problems through code.";
